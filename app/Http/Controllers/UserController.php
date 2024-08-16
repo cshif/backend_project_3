abc34afed6934f949fa800d30a810cd2456453bc
cshif <5175166+cshif@users.noreply.github.com>
2024-08-16 10:15:19 +0800

    feat: add user CRUD apis

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        $usersCount = $users->count();
+
+        if ($usersCount <= 0) {
+            return response()->json(['error' => 'error'], 400);
+        }
+
+        return response()->json($users, 200);
+    }
+
+    public function show(int $id)
+    {
+        $user = User::find($id);
+
+        if (! $user) {
+            return response()->json(['error' => 'No user found.'], 404);
+        }
+
+        return response()->json($user, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::create($request->input());
+
+        if (! $user) {
+            return response()->json(['error' => 'error'], 400);
+        }
+
+        return response()->json($user, 201);
+    }
+
+    public function update(Request $request, int $id)
+    {
+        $user = User::find($id);
+
+        if (! $user) {
+            return response()->json(['error' => 'No user found.'], 404);
+        }
+
+        $user->fill($request->all());
+        $user->save();
+
+        return response()->json($user, 200);
+    }
+
+    public function destroy(Request $request, int $id)
+    {
+        $user = User::find($id);
+
+        if (! $user) {
+            return response()->json(['error' => 'No user found.'], 404);
+        }
+
+        $user->delete();
+
+        return response()->json(['status' => 'User delete successfully.'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::patch('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
